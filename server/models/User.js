2f1162ec99acb6e60fb2f8e4d26a9a649eb0d4fd
D L Narayana <killjoy7471@gmail.com>
2026-04-26 19:49:20 +0530

    fix: fallback JWT_EXPIRE to 7d if not set

--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 UserSchema.methods.generateToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE || '7d' });
 };
 
 module.exports = mongoose.model('User', UserSchema);
